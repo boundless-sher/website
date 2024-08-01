be3fe7182ff2cb41e5933f8c21a91d777b95e8da
Sherzod <thisisotajonov@gmail.com>
2024-08-01 11:44:58 +0500

    add image compresssion to project images

--- projects/models.py
+++ projects/models.py
@@ -1,4 +1,18 @@
 from django.db import models
+from PIL import Image, ImageOps
+from io import BytesIO
+from django.core.files import File
+
+
+
+def image_compression(image):
+  img = Image.open(image)
+  img = img.convert('RGB')
+  img_io = BytesIO()
+  img.save(img_io, 'JPEG', quality=60, optimize=True)
+  new_image = File(img_io, name=image.name)
+  return new_image 
+
 
 # Create your models here.
 class Project(models.Model):
@@ -8,6 +22,13 @@ class Project(models.Model):
   image = models.ImageField()
   created = models.DateTimeField(auto_now_add=True)
   updated = models.DateTimeField(auto_now=True)
+
+
+  def save(self, *args, **kwargs):
+    self.image = image_compression(self.image)
+    super().save(*args, **kwargs)
+
+
 
   def __str__(self):
     return self.title
